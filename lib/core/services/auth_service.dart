2e10295553d7e4c45cae66f2ff9a9f37b64ce3b0
Christoph Fröhlich <christophfroehlich99@web.de>
2026-01-26 14:38:52 +0100

    fix: google api second fix login

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   User? get currentUser => _auth.currentUser;
 
@@ -37,34 +35,8 @@ class AuthService {
     }
   }
 
-  Future<UserCredential> signInWithGoogle() async {
-    try {
-
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-
-      if (googleUser == null) {
-        throw Exception('Google Sign-In abgebrochen');
-      }
-
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication;
-
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      return await _auth.signInWithCredential(credential);
-    } catch (e) {
-      throw Exception('Google Sign-In fehlgeschlagen: $e');
-    }
-  }
-
   Future<void> signOut() async {
-    await Future.wait([
-      _auth.signOut(),
-      _googleSignIn.signOut(),
-    ]);
+    await _auth.signOut();
   }
 
   Future<void> sendPasswordResetEmail(String email) async {
